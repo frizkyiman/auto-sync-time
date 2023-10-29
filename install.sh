7dd917bcb2ef76002e54f50f6511261191dc9625
frizkyiman <110967316+frizkyiman@users.noreply.github.com>
2023-10-29 14:29:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/sbin/sync_time.sh -O /sbin/sync_time.sh && chmod +x /sbin/sync_time.sh
 wget --no-check-certificate https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/usr/bin/clock -O /usr/bin/clock && chmod +x /usr/bin/clock
+
+cat <<'EOF' >/etc/init.d/sync_time
+#!/bin/sh /etc/rc.common
+
+START=99
+
+start() {
+    /sbin/sync_time.sh
+}
+EOF
+chmod +x /etc/init.d/sync_time
+
 rm /tmp/install.sh
